70acff39f4298e39c61546a3bb61699c7fc59149
M.Sadegh Zolghadr <zolghadr@outlook.com>
2019-01-05 18:24:41 +0330

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCKZw4q8gz4vOLlwaNwZJbFsMWBzdnd5Dg',
+    authDomain: 'demo1-oshop.firebaseapp.com',
+    databaseURL: 'https://demo1-oshop.firebaseio.com',
+    projectId: 'demo1-oshop',
+    storageBucket: 'demo1-oshop.appspot.com',
+    messagingSenderId: '255791030176'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCKZw4q8gz4vOLlwaNwZJbFsMWBzdnd5Dg',
+    authDomain: 'demo1-oshop.firebaseapp.com',
+    databaseURL: 'https://demo1-oshop.firebaseio.com',
+    projectId: 'demo1-oshop',
+    storageBucket: 'demo1-oshop.appspot.com',
+    messagingSenderId: '255791030176'
+  }
 };
 
 /*
